dc87458ad32deea1a192b85b2af77f99fefbf9a5
msb <msb20sobhi@gmail.com>
2024-08-28 15:45:34 +0300

    fastAPI app

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import joblib
 
 app = FastAPI()
-
-
+# GET request
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+def read_root():
+    return {"message": "Welcome to Tuwaiq Academy"}
+# get request
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+model = joblib.load('knn_model.joblib')
+scaler = joblib.load('Models/scaler.joblib')
+
+
+
+from pydantic import BaseModel
+# Define a Pydantic model for input data validation
+class InputFeatures(BaseModel):
+    appearance: int
+    games_injured: int
+    award: int
+    highest_value: int
+    goals_total: int
+    assists_total: int
+    yellow_cards_total: int
+    
+
+def preprocessing(input_features: InputFeatures):
+    dict_f = {
+        'appearance': input_features.appearance,
+        'games_injured': input_features.games_injured,
+        'award': input_features.award,
+        'highest_value': input_features.highest_value, 
+        'goals_total': input_features.goals_total,
+        'assists_total': input_features.assists_total,
+        'yellow_cards_total': input_features.yellow_cards_total
+        
+}
+    features_list = [dict_f[key] for key in sorted(dict_f)]
+# Scale the input features
+    scaled_features = scaler.transform([list(dict_f.values
+    ())])
+    return scaled_features
+
+
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    data = preprocessing(input_features)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
+
